b634aa670c2d856d37407666b8b46a828056a338
msacces_guruji <159617162+msaccesguruji@users.noreply.github.com>
2025-04-13 00:54:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -241,8 +241,6 @@ if uploaded_file:
             st.info("Generating Amazon Feed...")
             token = get_amazon_access_token()
             json_feed = generate_amazon_json_feed(file_stem, image_url)
-            st.code(json_feed, language='json')
-
             st.info("Submitting Feed to Amazon...")
             feed_id = submit_amazon_json_feed(json_feed, token)
             st.success(f"✅ Feed Submitted to Amazon — Feed ID: {feed_id}")
